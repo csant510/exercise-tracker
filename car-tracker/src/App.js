b5cd67c63af1657757d9dcb615ea215e9b0c43fd
cjs83 <james.sant83@gmail.com>
2019-10-10 19:11:25 -0700

    addig navbar logic

--- car-tracker/src/App.js
+++ car-tracker/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/navbar.component"
+import MaintenanceList from "./components/exercises-list.component";
+import EditMaintenance from "./components/edit-exercise.component";
+import CreateMaintenance from "./components/create-exercise.component";
+import CreateCar from "./components/create-user.component";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-  wtf
+    <Router>
+    <div className="container">
+    <Navbar />
+    <br/>
+    <Route path="/" exact component={MaintenanceList} />
+    <Route path="/edit/:id" component={EditMaintenance} />
+    <Route path="/create" component={CreateMaintenance} />
+    <Route path="/car" component={CreateCar} />
     </div>
+  </Router>
   );
 }
 
