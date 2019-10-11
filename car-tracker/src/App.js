61d54dcc10d52baac345ef316b035dc0d02b32c6
cjs83 <james.sant83@gmail.com>
2019-10-11 10:58:35 -0700

    made updates create maitance fourms and installed date picker

--- car-tracker/src/App.js
+++ car-tracker/src/App.js
@@ -5,19 +5,19 @@ import MaintenanceList from "./components/Maintenance-list.component";
 import EditMaintenance from "./components/edit-Maintenance.component";
 import CreateMaintenance from "./components/create-Maintenance.component";
 import CreateCar from "./components/create-car.component";
-
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+//import './App.css';
 
 function App() {
   return (
     <Router>
     <div className="container">
-    <Navbar />
-    <br/>
-    <Route path="/" exact component={MaintenanceList} />
-    <Route path="/edit/:id" component={EditMaintenance} />
-    <Route path="/create" component={CreateMaintenance} />
-    <Route path="/car" component={CreateCar} />
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={MaintenanceList} />
+      <Route path="/edit/:id" component={EditMaintenance} />
+      <Route path="/create" component={CreateMaintenance} />
+      <Route path="/car" component={CreateCar} />
     </div>
   </Router>
   );
--- car-tracker/src/components/create-Maintenance.component.js
+++ car-tracker/src/components/create-Maintenance.component.js
@@ -1,6 +1,87 @@
 import React, { Component } from 'react';
 
 export default class CreateMaintenance extends Component{
+    constructor(props){
+        super(props);
+        this.onChangeModel = this.onChangeModel.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onChangeOil = this.onChangeOil.bind(this);
+        this.onChangeFluid = this.onChangeFluid.bind(this);
+        this.onChangeTireRoation = this.onChangeTireRoation.bind(this);
+        this.onChangePrice = this.onChangePrice.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+
+        this.sate = {
+            model: '',
+            date: new Date(),
+            oil: false,
+            fluid: false,
+            tireRoation: false,
+            price: 0,
+            description: '',
+            models:[]
+        }
+    }
+    onChangeModel(e){
+        this.setState({
+            model: e.target.value
+        });
+    }
+    onChangeDate(date){
+        this.setState({
+            date: date
+        });
+    }
+    onChangeOil(e){
+        this.setState({
+            oil: e.target.value
+        });
+    }
+    onChangeFluid(e){
+        this.setState({
+            fluid: e.target.value
+        });
+    }
+    onChangeTireRoation(e){
+        this.setState({
+            tireRoation: e.target.value
+        });
+    }
+    onChangePrice(e){
+        this.setState({
+            price: e.target.value
+        });
+    }
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+    
+        const Maintenance = {
+          model: this.state.model,
+          date: this.state.date,
+          oil: this.sate.oil,
+          fluid: this.sate.fluid,
+          tireRoation: this.state.tireRoation,
+          price: this.state.price,
+          description: this.state.description,
+        }
+    
+        console.log(Maintenance);
+    
+    //    axios.post('http://localhost:5000/exercises/add', Maintenance)
+    //      .then(res => console.log(res.data));
+    
+        window.location = '/';
+      }
+    
+
     render(){
         return (
             <div>
