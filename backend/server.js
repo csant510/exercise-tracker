a65b6f07d1c072d4008be769c00b3818913ffa8e
cjs83 <james.sant83@gmail.com>
2019-10-22 17:35:54 -0700

    added users controllers and api route

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 
 //exercisetracker
@@ -33,10 +34,10 @@ connection.once('open', () => {
 
 
 const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+
 
 app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', require('./routes/api/users'));
 
 
 
