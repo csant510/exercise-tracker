9ddf9c42f12f9f7b29c352d3250319768da07d69
cjs83 <james.sant83@gmail.com>
2019-10-22 17:14:33 -0700

    chaing user models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
-  },
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {type: String, required: true, lowercase: true, unique: true},
+  password: String
 }, {
-  timestamps: true,
+  timestamps: true
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    // remove the password property when serializing doc to JSON
+    delete ret.password;
+    return ret;
+  }
+});
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
